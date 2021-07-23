20fe258b93006f6ac23fc09019a6cc24acac455d
blachocolat <blachocolat@gmail.com>
2021-07-23 14:21:47 +0900

    Add Chart.js

---
+++ src/components/ImagePieChart.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { Component, Mixins, Prop } from 'vue-property-decorator'
+import Chart from 'chart.js'
+import { Pie } from 'vue-chartjs'
+
+@Component
+export default class ImagePieChart extends Mixins(Pie) {
+  @Prop({ type: Object, required: true }) chartData!: Chart.ChartData
+  @Prop({ type: Object }) options?: Chart.ChartOptions
+
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -5,6 +5,11 @@
 
       <v-divider />
 
+      <image-pie-chart
+        :chartData="datacollection"
+        :options="{ cutoutPercentage: 0 }"
+      />
+
       <v-card-actions>
         <v-btn depressed color="primary">テスト</v-btn>
       </v-card-actions>
@@ -14,9 +19,29 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import ImagePieChart from '@/components/ImagePieChart.vue'
 
-@Component
-export default class App extends Vue {}
+@Component({
+  components: {
+    ImagePieChart,
+  },
+})
+export default class App extends Vue {
+  datacollection = {
+    labels: [this.getRandomInt(), this.getRandomInt()],
+    datasets: [
+      {
+        label: 'Data One',
+        backgroundColor: ['#087979', '#887979', '#f87979'],
+        data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt()],
+      },
+    ],
+  }
+
+  getRandomInt() {
+    return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+  }
+}
 </script>
 
 <style lang="scss">
