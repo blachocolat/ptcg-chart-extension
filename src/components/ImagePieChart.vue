722b2360593263bbead77cefe79723fc6d93fd92
blachocolat <blachocolat@gmail.com>
2021-07-24 00:19:45 +0900

    Adjust image sizing & positioning

--- src/components/ImagePieChart.vue
+++ src/components/ImagePieChart.vue
@@ -15,7 +15,11 @@ export default class ImagePieChart extends Vue {
 
   mounted() {
     const data = {
-      series: [2, 1, 1],
+      // series: [1],
+      // series: [1, 1, 1],
+      // series: [2, 1, 1],
+      // series: [1, 2, 1],
+      series: [8, 6, 4, 2, 1, 1],
     }
     const sum = (a: number, b: number) => {
       return a + b
@@ -27,36 +31,70 @@ export default class ImagePieChart extends Vue {
       },
     })
 
+    const baseWidth = 400
+    const baseHeight = 560
+
     this.chart.on('draw', (context: any) => {
       if (context.type == 'slice') {
         console.log(context)
+
         const imgId = `img-${Math.random().toString(36).substr(2, 8)}`
-        const baseWidth = 400
-        const baseHeight = 559
-        const centerAngle =
-          (context.endAngle - context.startAngle) / 2 + context.startAngle
-        const offsetX =
-          (context.radius / 2) * Math.sin(centerAngle * (Math.PI / 180)) + 5
-        const offsetY =
-          (context.radius / 2) * -Math.cos(centerAngle * (Math.PI / 180)) + 5
-        console.log(`sin(${centerAngle}) = (${offsetX}, ${offsetY})`)
+
+        const angleList = [
+          context.startAngle,
+          context.endAngle,
+          0,
+          90,
+          180,
+          270,
+        ]
+        let minX = 0
+        let minY = 0
+        let maxX = 0
+        let maxY = 0
+
+        angleList.some((angle: number) => {
+          if (angle < context.startAngle) {
+            return false // continue
+          }
+          if (context.endAngle < angle) {
+            return true // break
+          }
+
+          const x = Math.sin(angle * (Math.PI / 180))
+          const y = -Math.cos(angle * (Math.PI / 180))
+          minX = Math.min(minX, x)
+          minY = Math.min(minY, y)
+          maxX = Math.max(maxX, x)
+          maxY = Math.max(maxY, y)
+        })
+
+        const scale = Math.max(maxX - minX, maxY - minY) / 2
+        const width = baseWidth * scale
+        const height = baseHeight * scale
+        const offsetX = context.radius * ((minX + maxX) / 2 - (scale - 1))
+        const offsetY = context.radius * ((minY + maxY) / 2 - (scale - 1))
+        console.log(`(${minX}, ${minY}) -> (${maxX}, ${maxY})`)
+        console.log(`(${offsetX}, ${offsetY}) * ${scale}`)
 
         const svgNS = 'http://www.w3.org/2000/svg'
         const defs = document.createElementNS(svgNS, 'defs')
+
         const pattern = document.createElementNS(svgNS, 'pattern')
         pattern.setAttribute('id', imgId)
         pattern.setAttribute('patternUnits', 'userSpaceOnUse')
-        pattern.setAttribute('width', `${baseWidth}`)
-        pattern.setAttribute('height', `${baseHeight}`)
+        pattern.setAttribute('x', `${offsetX}`)
+        pattern.setAttribute('y', `${offsetY}`)
+        pattern.setAttribute('width', `${width}`)
+        pattern.setAttribute('height', `${height}`)
+
         const image = document.createElementNS(svgNS, 'image')
         image.setAttribute(
           'href',
           'https://www.pokemon-card.com/assets/images/card_images/large/SM11a/036912_P_RIZADONTERUNAGX.jpg'
         )
-        image.setAttribute('x', `${offsetX}`)
-        image.setAttribute('y', `${offsetY}`)
-        image.setAttribute('width', `${baseWidth}`)
-        image.setAttribute('height', `${baseHeight}`)
+        image.setAttribute('width', `${width}`)
+        image.setAttribute('height', `${height}`)
 
         pattern.appendChild(image)
         defs.appendChild(pattern)
